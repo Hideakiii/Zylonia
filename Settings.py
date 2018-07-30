4284fa611d2dfc7ab2096754abe32e4511179a66
Hideakiii <38570881+Hideakiii@users.noreply.github.com>
2018-07-30 21:41:35 +0200

    Create Settings.py

---
+++ Settings.py
@@ -0,0 +1,30 @@
+
+## Game settings
+
+width = 1280
+height = 860
+fps = 60
+game_title = "Zylonia"
+
+## Platforms
+platform_list = [(width / 5 - 50 , height * 3 / 4, 100, 20),
+                 (0 ,height - 50 , width ,50),
+                 (150 , height - 300 , 100, 20)]
+
+## Player Properties
+player_acc = 0.5
+player_friction = -0.12
+gravity = 0.5
+
+## Colors
+
+white = (255,255,255)
+black = (0,0,0)
+grey = (15,15,15)
+blue = (0,0,255)
+light_blue = (0,0,200)
+red = (255,0,0)
+light_red = (200,0,0)
+green = (0,255,0)
+
+
