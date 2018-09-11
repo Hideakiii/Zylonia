4a220e6f2e03f915d101f53eaafab62693dde54f
Hideakiii <38570881+Hideakiii@users.noreply.github.com>
2018-09-11 17:00:38 +0200

    Update Settings.py

--- Settings.py
+++ Settings.py
@@ -6,8 +6,13 @@ HEIGHT = 720
 FPS = 60
 GAME_TITLE = "Zylonia"
 HS_FILE = "highscore.txt"
-#SPRITESHEET = "Charakters_v1.png"
 SPRITESHEET = "Charakter_v4.png"
+
+## Flying platforms properties      ### Hier wurden alle verwendeten Variabeln von Fly_plat erstellt
+FLYPLAT_ACC = 0.5                   ### trotz dem gibt es mir den error : UnboundLocalError ("local variable 'FFLYPLAT_ACC' referenced before assignment ',)
+FLYPLAT_GRAV = 0.4
+FLYPLAT_MOVE = 20
+FLYPLAT_FRICTION = -0.12
 
 ## Platforms
 platform_list = [(WIDTH / 5 - 50 , HEIGHT * 3 / 4, 100, 20),
@@ -15,6 +20,14 @@ platform_list = [(WIDTH / 5 - 50 , HEIGHT * 3 / 4, 100, 20),
                  (150 , HEIGHT - 300 , 100, 20),
                  (400 , 350 , 150, 20),
                  (600 , 300 , 100, 20)]
+platform_list_2 = [(0 ,HEIGHT - 50 , WIDTH ,50),
+                    (20 , 510 , 250 , 20),
+                    (470 , 320 , 370 , 20),
+                    (290 , 390 , 210 , 20)]
+Flyplat_list = [
+                    (500 , 600 , 70 , 20)]
+                    #(450 , 300 , 350 , 20),
+                    #(300 , 400 , 220 , 20)]
 
 ## Player Properties
 player_acc = 0.5
@@ -23,16 +36,12 @@ gravity = 0.5
 JUMP_POWER = 12
 
 ## Colors
-
 white = (255,255,255)
 black = (0,0,0)
 grey = (15,15,15)
-blue = (0,0,255)
-light_blue = (0,0,200)
+dark_blue = (0,0,180)
+blue = (0,0,200)
+light_blue = (0,0,255)
 red = (255,0,0)
 light_red = (200,0,0)
 green = (0,255,0)
-
-
-
-
